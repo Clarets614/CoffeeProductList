de2dabedcaa3d0d705c55a36e71ad31e6327d26c
Clare S <Clare.Stock@gmail.com>
2024-05-30 23:35:04 -0400

    first commit

---
+++ CoffeeProductList/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using CoffeeProductList.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeProductList.Controllers
+{
+    public class ProductsController : Controller
+    {
+        ProductsDbContext dbContext = new ProductsDbContext();
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Product> result = dbContext.Products.ToList();
+            return View(result);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Product result = dbContext.Products.FirstOrDefault(p => p.Id == id);
+            return View(result);   
+        }
+
+        [HttpPost]
+        //public IActionResult Details(string category)
+        //{
+        //    string categories = dbContext.Products.Select(p => p.Category).Distinct().ToList();
+        //    Product products = (Product)dbContext.Products.Where(p => p.Category == category);
+
+        //}
+        public IActionResult Details(string category)
+        {
+            List<Product> result = dbContext.Products.Where(p => p.Category == category).ToList();
+            return View(result);
+        }
+
+    }
+}
